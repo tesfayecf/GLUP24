445d502fb84377a8595af48b475d047a379d2cb3
tesfayecf <90719152+tesfayecf@users.noreply.github.com>
2024-03-29 08:58:03 +0100

    Update study.py

--- study.py
+++ study.py
@@ -1,46 +1,41 @@
-# import optuna
-# from optuna.samplers import TPESampler
+from optuna import create_study 
+from train import train_model
 
-# def objective(trial):
-#     # Define the hyperparameters to be optimized
-#     hidden_units = trial.suggest_int("hidden_units", 16, 128)
-#     embedding_size = trial.suggest_int("embedding_size", 16, 128)
+MODEL_NAME = "LSTM"
+MODEL_VERSION = 1
 
-#     # Call your training function with the suggested hyperparameters
-#     mae, mse, rmse, r2 = train_function(
-#         hidden_units=hidden_units,
-#         embedding_size=embedding_size,
-#         # Pass other required arguments
-#     )
+def objective(trial):
+    
+    model_name = trial.suggest_categorical("model_name", ["LSTM", "GRU"])
+    
+    hidden_units = trial.suggest_int("hidden_units", 16, 128, step=16)
+    embedding_size = trial.suggest_int("embedding_size", 16, 256, step=16)
+    
+    sequence_size = trial.suggest_int("sequence_size", 6, 30, step=6)
+    
+    learning_rate = trial.suggest_float("learning_rate", 1e-4, 1e-2, log=True) 
+    # epochs 
+    optimizer = trial.suggest_categorical("optimizer", ["Adam", "RMSprop", "SGD", "Adagrad", "Adadelta"])
+    loss = trial.suggest_categorical("loss", ["mse", "mae", "mape", "msle"])
+    batch_size = trial.suggest_int("batch_size", 32, 128, step=32)
+    
+    metric = train_model(
+        MODEL_NAME, MODEL_VERSION, 
+        ### MODEL ###
+        hidden_units=hidden_units, embedding_size=embedding_size, 
+        ### DATASET ###
+        sequence_size=sequence_size,
+        ### TRAINING ###
+        learning_rate=learning_rate, optimizer=optimizer, loss=loss, batch_size=batch_size
+    )
+    
+    return metric
 
-#     # Return the metric you want to optimize (e.g., rmse)
-#     return rmse
 
-# # Create a study object
-# study = optuna.create_study(
-#     study_name="lstm_hyperparameter_optimization",
-#     sampler=TPESampler(seed=42),  # Specify the sampler if needed
-#     direction="minimize",  # "minimize" or "maximize" based on your objective
-# )
+def main():
+    study = create_study(direction="minimize", storage="sqlite:///hyperparameter_search_1.db")
+    study.optimize(objective, n_trials=50)  # Adjust the number of trials as needed
+    pass
 
-# # Run the optimization
-# study.optimize(objective, n_trials=100)  # Adjust the number of trials as needed
-
-# # Get the best hyperparameters
-# best_trial = study.best_trial
-# best_hidden_units = best_trial.params["hidden_units"]
-# best_embedding_size = best_trial.params["embedding_size"]
-# print(f"Best hidden units: {best_hidden_units}")
-# print(f"Best embedding size: {best_embedding_size}")
-
-# with mlflow.start_run() as run:
-#     # Log the best hyperparameters
-#     mlflow.log_param("best_hidden_units", best_hidden_units)
-#     mlflow.log_param("best_embedding_size", best_embedding_size)
-
-#     # Log the best metric
-#     best_metric_value = best_trial.value
-#     mlflow.log_metric("best_rmse", best_metric_value)
-
-#     # Log any other relevant information
-#     # ...
+if __name__ == "__main__":
+    main()
