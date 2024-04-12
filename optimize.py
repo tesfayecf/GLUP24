ca182a7258a366cce2792a441e1316678f917f17
tesfayecf <90719152+tesfayecf@users.noreply.github.com>
2024-04-13 01:13:10 +0200

    clean optimize

--- optimize.py
+++ optimize.py
@@ -8,27 +8,21 @@ def objective(trial):
     
     model_name = trial.suggest_categorical("model_name", ["LSTM"])
     
-    model_version = trial.suggest_int("model_version", 1, 2, 3, 4)
+    model_version = trial.suggest_categorical("model_version", [1, 2, 3, 4])
     
-    # hidden_units = trial.suggest_int("hidden_units", 32, 64, 96, 128)
-    # embedding_size = trial.suggest_int("embedding_size", 16, 32, 64, 80)
-    hidden_units = trial.suggest_int("hidden_units", 16, 128, step=16)
-    embedding_size = trial.suggest_int("embedding_size", 16, 256, step=16)
+    hidden_units = trial.suggest_categorical("hidden_units", [32, 64, 96, 128])
+    embedding_size = trial.suggest_categorical("embedding_size", [16, 32, 64, 80])
     
-    num_blocks_residual = trial.suggest_int("num_blocks_residual", 1, 2, 3, 4)
-    dropout_rate = trial.suggest_float("dropout_rate", 0.1, 0.3, 0.5)
-    conv_filters = trial.suggest_int("conv_filters", 32, 64, 96, 128)
-    conv_kernel_size = trial.suggest_int("conv_kernel_size", 3, 5, 7)
+    num_blocks_residual = trial.suggest_categorical("num_blocks_residual", [1, 2, 3, 4])
+    dropout_rate = trial.suggest_float("dropout_rate", 0.1, 0.5)
+    conv_filters = trial.suggest_categorical("conv_filters", [32, 64, 96, 128])
+    conv_kernel_size = trial.suggest_categorical("conv_kernel_size", [3, 5, 7])
     
-    # sequence_size = trial.suggest_int("sequence_size", 12)
-    sequence_size = trial.suggest_int("sequence_size", 6, 30, step=6)
+    sequence_size = 12
     
     learning_rate = trial.suggest_float("learning_rate", 1e-4, 1e-2, log=True) 
-    # optimizer = trial.suggest_categorical("optimizer", ["Adam"])
-    # loss = trial.suggest_categorical("loss", ["mse", "mae", "mape", "msle"])
-    # batch_size = trial.suggest_int("batch_size", 32, 128, step=32)
-    optimizer = trial.suggest_categorical("optimizer", ["Adam", "RMSprop", "SGD", "Adagrad", "Adadelta"])
-    loss = trial.suggest_categorical("loss", ["mse", "mae", "mape", "msle"])
+    optimizer = trial.suggest_categorical("optimizer", ["Adam", "SGD", "Adagrad", "Adadelta"])
+    loss = trial.suggest_categorical("loss", ["mse", "mae"])
     batch_size = trial.suggest_int("batch_size", 32, 128, step=32)
     
     metric = train(
@@ -46,10 +40,10 @@ def objective(trial):
     return metric
 
 
-def main():
-    study = create_study(direction="minimize", storage="sqlite:///hyperparameter_search_1.db")
+def optimize():
+    study = create_study(direction="minimize")
     study.optimize(objective, n_trials=75)
     pass
 
 if __name__ == "__main__":
-    main()
+    optimize()
