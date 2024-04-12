8306b03c59226291211d96139b69e665f42da22c
tesfayecf <90719152+tesfayecf@users.noreply.github.com>
2024-04-12 23:41:19 +0200

    refactor and cleaning

--- optimize.py
+++ optimize.py
@@ -1,8 +1,8 @@
+from dotenv import load_dotenv
 from optuna import create_study 
-from train import train_model
+from train import train
 
-MODEL_NAME = "LSTM"
-MODEL_VERSION = 1
+load_dotenv()
 
 def objective(trial):
     
@@ -10,22 +10,28 @@ def objective(trial):
     
     model_version = trial.suggest_int("model_version", 1, 2, 3, 4)
     
-    hidden_units = trial.suggest_int("hidden_units", 32, 64, 96, 128)
-    embedding_size = trial.suggest_int("embedding_size", 16, 32, 64, 80)
+    # hidden_units = trial.suggest_int("hidden_units", 32, 64, 96, 128)
+    # embedding_size = trial.suggest_int("embedding_size", 16, 32, 64, 80)
+    hidden_units = trial.suggest_int("hidden_units", 16, 128, step=16)
+    embedding_size = trial.suggest_int("embedding_size", 16, 256, step=16)
+    
     num_blocks_residual = trial.suggest_int("num_blocks_residual", 1, 2, 3, 4)
     dropout_rate = trial.suggest_float("dropout_rate", 0.1, 0.3, 0.5)
     conv_filters = trial.suggest_int("conv_filters", 32, 64, 96, 128)
     conv_kernel_size = trial.suggest_int("conv_kernel_size", 3, 5, 7)
     
-    sequence_size = trial.suggest_int("sequence_size", 12)
+    # sequence_size = trial.suggest_int("sequence_size", 12)
+    sequence_size = trial.suggest_int("sequence_size", 6, 30, step=6)
     
     learning_rate = trial.suggest_float("learning_rate", 1e-4, 1e-2, log=True) 
-    # epochs 
-    optimizer = trial.suggest_categorical("optimizer", ["Adam"])
+    # optimizer = trial.suggest_categorical("optimizer", ["Adam"])
+    # loss = trial.suggest_categorical("loss", ["mse", "mae", "mape", "msle"])
+    # batch_size = trial.suggest_int("batch_size", 32, 128, step=32)
+    optimizer = trial.suggest_categorical("optimizer", ["Adam", "RMSprop", "SGD", "Adagrad", "Adadelta"])
     loss = trial.suggest_categorical("loss", ["mse", "mae", "mape", "msle"])
     batch_size = trial.suggest_int("batch_size", 32, 128, step=32)
     
-    metric = train_model(
+    metric = train(
         model_name, model_version, 
         ### MODEL ###
         hidden_units=hidden_units, embedding_size=embedding_size, 
