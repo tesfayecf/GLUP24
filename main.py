64186793051037f6c4a8f9ad28d125a3c64f0c6b
tesfayecf <90719152+tesfayecf@users.noreply.github.com>
2024-04-13 01:03:35 +0200

    main methods

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
-from train import train_model
+from dotenv import load_dotenv
+load_dotenv()
 
-def main():
+from train import train
+from test import test
+from evaluate import evaluate
+
+def train_model():
     model_name = "LSTM"
-    model_version = 5
+    model_version = 1
     
     hidden_units = 96
     embedding_size = 80
-    num_residual_blocks = 2
-    dropout_rate = 0.3
-    conv_filters = 64
-    conv_kernel_size = 3
-
     sequence_size = 12
     
     learning_rate = 0.001
-    epochs = 100
+    epochs = 1
     optimizer = "Adam"
     loss = "mae"
-    batch_size = 128
+    batch_size = 16
     
-    metric = train_model(
+    metric = train(
         model_name, model_version, 
         ### MODEL ###
         hidden_units=hidden_units, embedding_size=embedding_size, 
-        num_residual_blocks=num_residual_blocks, dropout_rate=dropout_rate, 
-        conv_filters=conv_filters, conv_kernel_size=conv_kernel_size,
         ### DATASET ###
         sequence_size=sequence_size,
         ### TRAINING ###
@@ -33,5 +31,13 @@ def main():
     
     return metric
 
+def test_model():
+    run_id = "e48c057ce2744afda3f64721b6a7301b"
+    test(run_id)
+
+def evaluate_model():
+    run_id = "e48c057ce2744afda3f64721b6a7301b"
+    evaluate(run_id)
+
 if __name__ == "__main__":
-    main()
+    evaluate_model() 
