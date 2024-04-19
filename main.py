0243d621ade9a1b390737dc3280db8f3998c8228
tesfayecf <90719152+tesfayecf@users.noreply.github.com>
2024-04-19 20:51:35 +0200

    use gpu if available

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from train import train
+import sys
+import tensorflow as tf
+
 from test import test
+from train import train
 from evaluate import evaluate
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+  # Restrict TensorFlow to only use the first GPU
+  try:
+    tf.config.set_visible_devices(gpus[0], 'GPU')
+    logical_gpus = tf.config.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+  except RuntimeError as e:
+    # Visible devices must be set before GPUs have been initialized
+    print(e)
 
 def train_model():
     model_name = "LSTM"
