cd9db19e93f991a636bf00e8a12b07b6bc7bb5df
tesfayecf <90719152+tesfayecf@users.noreply.github.com>
2024-05-08 21:14:34 +0200

    Update optimize.py

--- optimize.py
+++ optimize.py
@@ -11,10 +11,11 @@ def objective(trial: Trial):
     model_version = 1
     
     ### MODEL ###
-    hidden_units = trial.suggest_categorical("hidden_units", [64, 128, 256, 512])
-    num_layers = trial.suggest_categorical("num_layers", [1, 2, 3])
+    hidden_units = trial.suggest_categorical("hidden_units", [128, 256])
+    filters = trial.suggest_categorical("filters", [8, 16, 32, 64])
+    num_layers = trial.suggest_categorical("num_layers", [1, 2, 3, 4])
     num_heads = trial.suggest_categorical("num_heads", [32, 64, 128, 256])
-    dropout_rate = trial.suggest_categorical("dropout_rate", [0.0, 0.1, 0.25, 0.3, 0.5])
+    dropout = 0.1
     
     ### DATASET ###
     sequence_size = 12
@@ -24,18 +25,18 @@ def objective(trial: Trial):
     create_fatigue = True
     scale_data = True
     perform_pca = True
-    pca_components = trial.suggest_categorical("pca_components", [6, 7, 8, 9, 10])
+    pca_components = 8
 
     ### TRAINING ###
-    learning_rate = trial.suggest_categorical("learning_rate", [0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001]) 
-    optimizer = trial.suggest_categorical("optimizer", ["Adam", "SGD", "Adagrad", "Adadelta"])
-    loss = trial.suggest_categorical("loss", ["mse", "mae"])
-    batch_size = trial.suggest_int("batch_size", 32, 256, step=16)
+    learning_rate = trial.suggest_categorical("learning_rate", [0.01, 0.005, 0.001]) 
+    optimizer = "Adam"
+    loss = "mae"
+    batch_size = trial.suggest_categorical("batch_size", [32, 64, 128])
     
     metric = train(
         model_name, model_version, 
         ### MODEL ###
-        hidden_units=hidden_units, num_layers=num_layers, num_heads=num_heads, dropout_rate=dropout_rate,
+        hidden_units=hidden_units, filters=filters, num_layers=num_layers, num_heads=num_heads, dropout=dropout,
         ### DATASET ###
         sequence_size=sequence_size, prediction_time=prediction_time, validation_split=validation_split, 
         create_timestamp=create_timestamp, create_fatigue=create_fatigue, scale_data=scale_data, perform_pca=perform_pca, pca_components=pca_components,
@@ -52,7 +53,3 @@ def optimize():
                          study_name=f"{os.getenv('EXPERIMENT_NAME')}-{int(os.getenv('DATASET_NUMBER'))}-{int(os.getenv('PREDICTION'))}", 
                          storage=f"sqlite:///{os.getenv('EXPERIMENT_NAME')}-{int(os.getenv('DATASET_NUMBER'))}-{int(os.getenv('PREDICTION'))}.db")
     study.optimize(objective, n_trials=250, n_jobs=1, show_progress_bar=True)
-    pass
-
-if __name__ == "__main__":
-    optimize()
